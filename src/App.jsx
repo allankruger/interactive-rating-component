8d479c7befc637d7057b904ed662c364aa048d10
Allan Kruger <allan.kruger@compasso.com.br>
2022-06-07 00:22:27 +0100

    adds basic card structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import './global.css';
+import "./global.css";
+import styles from "./App.module.css";
 
 export function App() {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className={styles.card}>
+      <h1>How did we do?</h1>
+
+      <p>
+        Please let us know how we did with your support request. All feedback is
+        appreciated to help us improve our offering!
+      </p>
+
+      <ul className={styles.ratings}>
+        <li>1</li>
+        <li>2</li>
+        <li>3</li>
+        <li>4</li>
+        <li>5</li>
+      </ul>
+
+      <button className={styles.button}>Submit</button>
     </div>
   );
 }
